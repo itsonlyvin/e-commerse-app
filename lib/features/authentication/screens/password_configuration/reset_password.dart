11c5410c1d9ad1887ba4bb20ec0c1dc5e0faee76
vinayak <vinayakvichuvickey778@gmail.com>
2025-07-02 00:40:40 +0530

    added success_screen, reset & verify password

---
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:get/utils.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+class ResetPassword extends StatelessWidget {
+  const ResetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            icon: const Icon(CupertinoIcons.clear),
+            onPressed: () => Get.back(),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsetsGeometry.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Image
+              Image(
+                image: const AssetImage(TImages.deliveredEmailIllustration),
+                width: THelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// TItle & Subtitle
+              Text(
+                TTexts.changeYourPasswordTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              Text(
+                TTexts.changeYourPasswordSubTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              /// Button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(TTexts.done),
+                ),
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(TTexts.resendEmail),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
