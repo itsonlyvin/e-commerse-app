8c1eee85f302c7aed632ab05de07c7401d0358e9
vinayak <vinayakvichuvickey778@gmail.com>
2025-07-08 21:11:45 +0530

    carousal

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
+import 'package:t_store/common/widgets/images/t_rounded_images.dart';
 import 'package:t_store/common/widgets/text/section_heading.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:t_store/features/shop/screens/home/widgets/home_categories.dart';
+import 'package:t_store/features/shop/screens/home/widgets/promo_slider.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,41 +23,54 @@ class HomeScreen extends StatelessWidget {
           children: [
             /// Header
             TPrimaryHeaderContainer(
-                child: Column(
-              children: [
-                /// Appbar
-                THomeAppBar(),
-                SizedBox(
-                  height: TSizes.spaceBtwSections,
-                ),
+              child: Column(
+                children: [
+                  /// Appbar
+                  THomeAppBar(),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
 
-                /// Searchbar
-                TSearchContainer(
-                  text: "Search in Store",
-                ),
-                SizedBox(
-                  height: TSizes.spaceBtwSections,
-                ),
+                  /// Searchbar
+                  TSearchContainer(
+                    text: "Search in Store",
+                  ),
+                  SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  ),
 
-                /// Categories
-                Column(
-                  children: [
-                    /// Heading
-                    TSectionHeading(
-                      title: 'Popular Categories',
-                      showActionButton: false,
-                      textColor: TColors.white,
-                    ),
-                    SizedBox(
-                      height: TSizes.spaceBtwItems,
-                    ),
+                  /// Categories
+                  Column(
+                    children: [
+                      /// Heading
+                      TSectionHeading(
+                        title: 'Popular Categories',
+                        showActionButton: false,
+                        textColor: TColors.white,
+                      ),
+                      SizedBox(
+                        height: TSizes.spaceBtwItems,
+                      ),
 
-                    /// Categories
-                    THomeCategories()
-                  ],
-                ),
-              ],
-            )),
+                      /// Categories
+                      THomeCategories()
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            /// Body
+            Padding(
+              padding: EdgeInsetsGeometry.all(TSizes.defaultSpace),
+              child: TPromoSlider(
+                banners: [
+                  TImages.promoBanner1,
+                  TImages.promoBanner2,
+                  TImages.promoBanner3
+                ],
+              ),
+            ),
           ],
         ),
       ),
